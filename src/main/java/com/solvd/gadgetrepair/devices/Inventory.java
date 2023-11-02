046bc2b3eacbe468258c4e08b27112fe9e1d9a1a
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-01 23:48:25 -0400

    Update Inventory.java

--- src/main/java/com/solvd/gadgetrepair/devices/Inventory.java
+++ src/main/java/com/solvd/gadgetrepair/devices/Inventory.java
@@ -20,6 +20,20 @@ public class Inventory {
         }
     }
 
+    public void removePart(String part, int quantity) {
+        for (int i=0; i < parts.length; i++) {
+            if (parts[i] != null && parts[i].equals(part)) {
+                if (quantities[i] > quantity) {
+                    quantities[i] -= quantity;
+                } else {
+                    parts[i] = null;
+                    quantities[i] = 0;
+                }
+                break;
+            }
+        }
+    }
+
     public int getQuantity(String part) {
         for (int i = 0; i < parts.length; i++) {
             if (part.equals(parts[i])) {
