650812e9a5ad1eabf8c8a1515d3e2f3a400a3638
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-03 03:01:06 -0400

    Update Main.java

--- src/main/java/com/solvd/gadgetrepair/Main.java
+++ src/main/java/com/solvd/gadgetrepair/Main.java
@@ -1,29 +1,28 @@
 package com.solvd.gadgetrepair;
 
-import com.solvd.gadgetrepair.cost.Payment;
+import com.solvd.gadgetrepair.cost.Billing;
 import com.solvd.gadgetrepair.cost.RepairService;
 import com.solvd.gadgetrepair.devices.DeviceRepairInfo;
 import com.solvd.gadgetrepair.devices.Gadget;
 import com.solvd.gadgetrepair.devices.Inventory;
 import com.solvd.gadgetrepair.devices.ServiceRecord;
-import com.solvd.gadgetrepair.human.Customer;
-import com.solvd.gadgetrepair.human.Employee;
+import com.solvd.gadgetrepair.human.Person;
 import com.solvd.gadgetrepair.status.Notification;
 import com.solvd.gadgetrepair.status.RepairStatus;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // Create and initialize objects, can customize
-        Customer customer = new Customer("Max Stirner", "mstirner@example.com", "215-555-9640", "email");
-        Gadget gadget = new Gadget("Phone", "SN123456", "Camera lens cracked");
-        DeviceRepairInfo repairInfo = new DeviceRepairInfo("Phone", "screen", 100.00, 2);
+        Person.Customer customer = new Person.Customer("Max Stirner", "mstirner@example.com", "215-555-9640", "email");
+        Gadget gadget = new Gadget("Phone", "SN123456", "Cracked screen");
+        DeviceRepairInfo.PhoneRepair repairInfo = new DeviceRepairInfo.PhoneRepair("screen", 150.00);
         ServiceRecord[] repairRecords = {
-                new ServiceRecord("15/2/22", 250.0, "Screen replacement"),
-                new ServiceRecord("14/1/23", 100.0, "Battery replacement"),
+                new ServiceRecord("15/2/22", 250.00, "Screen replacement"),
+                new ServiceRecord("14/1/23", 100.00, "Battery replacement"),
                 new ServiceRecord("22/10/23", 75.00, "Software update")
         };
-        Notification notification = new Notification(customer, "Repair complete", "Your device is ready for pickup");
-        Payment payment = new Payment();
+        Notification notification = new Notification.Email(customer, "Repair complete", "Your device is ready for pickup");
+        Billing payment = new Billing.CreditCard();
 
 
         // Customer brings in a new device for repair
@@ -36,27 +35,17 @@ public class Main {
         System.out.println("Gadget status is " + repairStatus.getStatus(gadget));
 
         // Find an employee
-        Employee employee = new Employee("John Zerzan", "Phone technician", "Available");
-        System.out.println("Employee " + employee.getName() + " is " + employee.getAvailability());
+        Person.Employee employee = new Person.Employee("John Zerzan", "Phone technician", "Available");
+        System.out.println("Employee " + employee.getFullName() + " is " + employee.getAvailability());
         employee.setAvailability("Busy");
-        System.out.println(employee.getName() + " is now " + employee.getAvailability());
+        System.out.println(employee.getFullName() + " is now " + employee.getAvailability());
         repairStatus.markUnderRepair(gadget);
         System.out.println("Gadget status is " + repairStatus.getStatus(gadget));
 
-        // Estimate time and cost of repair
-        int estimatedTime = repairService.estimateRepairTime(repairInfo);
-        if (estimatedTime != -1) {
-            System.out.println("Estimated Repair Time for " + repairInfo.getGadgetType() + ": " + estimatedTime + " hours");
-        } else {
-            System.out.println("Device type not supported for repair.");
-        }
-
-        double cost = repairService.calculateRepairCost(repairInfo);
-        if (cost != -1.0) {
-            System.out.println("Calculated Repair Cost for " + repairInfo.getGadgetType() + ": $" + cost);
-        } else {
-            System.out.println("Device type not supported for repair.");
-        }
+        // Get time and cost estimates
+        int phoneRepairTime = repairInfo.estimateRepairTime();
+        double phoneRepairCost = repairInfo.calculateRepairCost();
+        System.out.println("Phone Repair - Time Estimate: " + phoneRepairTime + " hours, Cost: $" + phoneRepairCost);
 
         // Check inventory for necessary part
         Inventory inventory = new Inventory(250);
@@ -83,19 +72,17 @@ public class Main {
         System.out.println("Gadget status is " + repairStatus.getStatus(gadget));
 
         // Add a new service record to the customer
-        ServiceRecord newRecord = new ServiceRecord("30/10/23", 160.0, "Screen repair");
+        ServiceRecord newRecord = new ServiceRecord("30/10/23", 220.00, "Screen repair");
         customer.addRepairRecord(newRecord);
 
-        // Calculate total repair cost
-        payment.calculateCost(customer, customer.getRepairHistory().toArray(new ServiceRecord[0]));
+        // Update customer's repair history
+        customer.getRepairHistory().toArray(new ServiceRecord[0]);
 
         // Generate and send invoice
-        payment.generateInvoice(customer);
+        payment.calculateCost(customer, new ServiceRecord[]{newRecord});
         notification.sendNotification();
 
         // Process payment
-        String paymentMethod = "Credit card";
-        payment.setPaymentMethod(paymentMethod);
         payment.processPayment(customer);
     }
 }
