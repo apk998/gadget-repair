010464d0c2cab3a846a472c0d6a8785ee7d59817
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-07 01:41:51 -0500

    Update Notification.java

--- src/main/java/com/solvd/gadgetrepair/status/Notification.java
+++ src/main/java/com/solvd/gadgetrepair/status/Notification.java
@@ -1,59 +1,15 @@
 package com.solvd.gadgetrepair.status;
 
-import com.solvd.gadgetrepair.human.Person;
+import com.solvd.gadgetrepair.human.Customer;
 
-// Manages notifications and communication within gadget repair service
 public abstract class Notification {
-    protected Person.Customer recipient;
-    protected String subject;
-    protected String message;
+    protected Notifiable notifiable;
 
-    public Notification(Person.Customer recipient, String subject, String message) {
-        this.recipient = recipient;
-        this.subject = subject;
-        this.message = message;
+    public Notification(Notifiable notifiable) {
+        this.notifiable = notifiable;
     }
 
-    public abstract void sendNotification();
-
-    public Person.Customer getRecipient() {
-        return recipient;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public static class Email extends Notification {
-        public Email(Person.Customer recipient, String subject, String message) {
-            super(recipient, subject, message);
-        }
-
-        @Override
-        public void sendNotification() {
-            String email = recipient.getEmail();
-            // Pretend an email is actually being sent
-            System.out.println("Sending an email notification to: " + email);
-            System.out.println("Subject: " + subject);
-            System.out.println("Message: " + message);
-        }
-    }
-
-    public static class SMS extends Notification {
-        public SMS(Person.Customer recipient, String subject, String message) {
-            super(recipient, subject, message);
-        }
-
-        @Override
-        public void sendNotification() {
-            String phoneNumber = recipient.getPhoneNumber();
-            // Pretend an SMS is actually being sent
-            System.out.println("Sending an SMS notification to: " + phoneNumber);
-            System.out.println("Message: " + message);
-        }
+    public void sendNotification(Customer recipient, String subject, String message) {
+        notifiable.sendNotification(recipient, subject, message);
     }
 }
