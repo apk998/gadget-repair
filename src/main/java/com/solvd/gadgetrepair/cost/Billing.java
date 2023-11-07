6eb6e360733d387d5f7517a712822f8f57ba6c80
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-07 01:26:54 -0500

    Update Billing.java

--- src/main/java/com/solvd/gadgetrepair/cost/Billing.java
+++ src/main/java/com/solvd/gadgetrepair/cost/Billing.java
@@ -1,7 +1,7 @@
 package com.solvd.gadgetrepair.cost;
 
 import com.solvd.gadgetrepair.devices.ServiceRecord;
-import com.solvd.gadgetrepair.human.Person;
+import com.solvd.gadgetrepair.human.Customer;
 
 // Calculates total cost, generates invoices, and processes payments
 public abstract class Billing {
@@ -13,55 +13,12 @@ public abstract class Billing {
         paymentMethod = "Credit card";   // default payment method
     }
 
-    public void calculateCost(Person.Customer customer, ServiceRecord[] repairRecords) {
+    public void calculateCost(Customer customer, ServiceRecord[] repairRecords) {
         totalCost = 0.0;
         if (repairRecords.length > 0) {
             totalCost = repairRecords[repairRecords.length - 1].getRepairCost();
         }
     }
 
-    public abstract void processPayment (Person.Customer customer);
-
-    public static class CreditCard extends Billing {
-        public CreditCard() {
-            paymentMethod = "Credit card";
-        }
-
-        @Override
-        public void processPayment (Person.Customer customer) {
-            // Pretend customer has paid with credit card
-            System.out.println("Processing credit card payment for: " + customer.getFullName());
-            System.out.println("Total amount: $" + totalCost);
-            System.out.println("Payment method: " + paymentMethod);
-        }
-    }
-
-    public static class PayPal extends Billing {
-        public PayPal() {
-            paymentMethod = "PayPal";
-        }
-
-        @Override
-        public void processPayment(Person.Customer customer) {
-            // Pretend customer has paid via PayPal
-            System.out.println("Processing credit card payment for: " + customer.getFullName());
-            System.out.println("Total amount: $" + totalCost);
-            System.out.println("Payment method: " + paymentMethod);
-        }
-    }
-
-    public static class Cash extends Billing {
-        public Cash() {
-            paymentMethod = "Cash";
-        }
-
-        @Override
-        public void processPayment(Person.Customer customer) {
-            // Pretend customer has paid with cash
-            System.out.println("Processing credit card payment for: " + customer.getFullName());
-            System.out.println("Total amount: $" + totalCost);
-            System.out.println("Payment method: " + paymentMethod);
-        }
-    }
+    public abstract void processPayment (Customer customer);
 }
-
