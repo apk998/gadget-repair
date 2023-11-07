0deaf4b78f10ab124134d4f6187a4176fac3d877
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-07 01:27:37 -0500

    Update and rename RepairService.java to Payable.java

---
+++ src/main/java/com/solvd/gadgetrepair/cost/Payable.java
@@ -0,0 +1,9 @@
+package com.solvd.gadgetrepair.cost;
+
+import com.solvd.gadgetrepair.devices.ServiceRecord;
+import com.solvd.gadgetrepair.human.Customer;
+
+public interface Payable {
+    void calculateCost(Customer customer, ServiceRecord[] repairRecords);
+    void processPayment(Customer customer);
+}
