7a83f75482b999c2ec3cb99db20477af3fd36eeb
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-07 01:43:54 -0500

    Update Main.java

--- src/main/java/com/solvd/gadgetrepair/Main.java
+++ src/main/java/com/solvd/gadgetrepair/Main.java
@@ -1,33 +1,31 @@
 package com.solvd.gadgetrepair;
 
 import com.solvd.gadgetrepair.cost.Billing;
-import com.solvd.gadgetrepair.cost.RepairService;
-import com.solvd.gadgetrepair.devices.DeviceRepairInfo;
-import com.solvd.gadgetrepair.devices.Gadget;
-import com.solvd.gadgetrepair.devices.Inventory;
-import com.solvd.gadgetrepair.devices.ServiceRecord;
-import com.solvd.gadgetrepair.human.Person;
-import com.solvd.gadgetrepair.status.Notification;
+import com.solvd.gadgetrepair.cost.CreditCard;
+import com.solvd.gadgetrepair.devices.*;
+import com.solvd.gadgetrepair.human.Customer;
+import com.solvd.gadgetrepair.human.Employee;
+import com.solvd.gadgetrepair.status.Email;
+import com.solvd.gadgetrepair.status.Notifiable;
 import com.solvd.gadgetrepair.status.RepairStatus;
 
 public class Main {
     public static void main(String[] args) {
         // Create and initialize objects, can customize
-        Person.Customer customer = new Person.Customer("Max Stirner", "mstirner@example.com", "215-555-9640", "email");
+        Customer customer = new Customer("Max Stirner", "mstirner@example.com", "215-555-9640", "email");
         Gadget gadget = new Gadget("Phone", "SN123456", "Cracked screen");
-        DeviceRepairInfo.PhoneRepair repairInfo = new DeviceRepairInfo.PhoneRepair("screen", 150.00);
+        PhoneRepair repairInfo = new PhoneRepair("screen", 150.00);
         ServiceRecord[] repairRecords = {
                 new ServiceRecord("15/2/22", 250.00, "Screen replacement"),
                 new ServiceRecord("14/1/23", 100.00, "Battery replacement"),
                 new ServiceRecord("22/10/23", 75.00, "Software update")
         };
-        Notification notification = new Notification.Email(customer, "Repair complete", "Your device is ready for pickup");
-        Billing payment = new Billing.CreditCard();
+        Notifiable emailMethod = new Email(null);
+        Billing payment = new CreditCard();
 
 
         // Customer brings in a new device for repair
         System.out.println(customer.getFullName() + " brings in a " + gadget.getGadgetType() + " with a " + gadget.getProblemDescription() + " for repair.");
-        RepairService repairService = new RepairService();
 
         // Update gadget repair status
         RepairStatus repairStatus = new RepairStatus();
@@ -35,7 +33,7 @@ public class Main {
         System.out.println("Gadget status is " + repairStatus.getStatus(gadget));
 
         // Find an employee
-        Person.Employee employee = new Person.Employee("John Zerzan", "Phone technician", "Available");
+        Employee employee = new Employee("John Zerzan", "Phone technician", "Available");
         System.out.println("Employee " + employee.getFullName() + " is " + employee.getAvailability());
         employee.setAvailability("Busy");
         System.out.println(employee.getFullName() + " is now " + employee.getAvailability());
@@ -48,8 +46,8 @@ public class Main {
         System.out.println("Phone Repair - Time Estimate: " + phoneRepairTime + " hours, Cost: $" + phoneRepairCost);
 
         // Check inventory for necessary part
-        Inventory inventory = new Inventory(250);
-        inventory.addPart("screen", 10);
+        Inventory inventory = new Inventory(Inventory.MAX_CAPACITY);
+        inventory.addPart("screen", Inventory.DEFAULT_PART_QUANTITY);
         int screenStock = inventory.getQuantity("screen");
         if (screenStock > 0) {
             System.out.println("Available screens in inventory: " + screenStock);
@@ -72,7 +70,7 @@ public class Main {
         System.out.println("Gadget status is " + repairStatus.getStatus(gadget));
 
         // Add a new service record to the customer
-        ServiceRecord newRecord = new ServiceRecord("30/10/23", 220.00, "Screen repair");
+        ServiceRecord newRecord = new ServiceRecord("30/10/23", 237.60, "Screen repair");
         customer.addRepairRecord(newRecord);
 
         // Update customer's repair history
@@ -80,7 +78,7 @@ public class Main {
 
         // Generate and send invoice
         payment.calculateCost(customer, new ServiceRecord[]{newRecord});
-        notification.sendNotification();
+        emailMethod.sendNotification(customer, "Repair complete", "Your device is ready for pickup");
 
         // Process payment
         payment.processPayment(customer);
