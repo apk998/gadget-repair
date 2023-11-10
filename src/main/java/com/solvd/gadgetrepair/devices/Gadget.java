5db32a3ae541438305c84c206bd1db35bd950074
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-10 03:03:52 -0500

    Update Gadget.java

--- src/main/java/com/solvd/gadgetrepair/devices/Gadget.java
+++ src/main/java/com/solvd/gadgetrepair/devices/Gadget.java
@@ -1,15 +1,27 @@
 package com.solvd.gadgetrepair.devices;
 
+import com.solvd.gadgetrepair.exceptions.GadgetException;
+
+import java.util.Arrays;
+
 // Represents the gadgets brought in to be repaired
 public class Gadget {
     private String gadgetType;
     private String serialNumber;
     private String problemDescription;
 
-    public Gadget(String gadgetType, String serialNumber, String problemDescription) {
+    public Gadget(String gadgetType, String serialNumber, String problemDescription) throws GadgetException {
+        if (!isGadgetAccepted(gadgetType)) {
+            throw new GadgetException("Unaccepted gadget type: " + gadgetType);
+        }
         this.gadgetType = gadgetType;
         this.serialNumber = serialNumber;
         this.problemDescription = problemDescription;
+    }
+
+    private boolean isGadgetAccepted(String gadgetType) {
+        String[] acceptedTypes = {"Phone", "Laptop", "TV"};
+        return Arrays.asList(acceptedTypes).contains(gadgetType);
     }
 
     public String getGadgetType() {
