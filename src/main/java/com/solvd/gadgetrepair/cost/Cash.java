f45a1d00b9baf88118f100f968608bde33f33545
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-14 01:45:32 -0500

    Update Cash.java

--- src/main/java/com/solvd/gadgetrepair/cost/Cash.java
+++ src/main/java/com/solvd/gadgetrepair/cost/Cash.java
@@ -1,17 +1,20 @@
 package com.solvd.gadgetrepair.cost;
 
 import com.solvd.gadgetrepair.human.Customer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Cash extends Billing implements Payable {
+    private static final Logger LOGGER=LogManager.getLogger(Cash.class);
     public Cash() {
-        paymentMethod = "Cash";
+        setPaymentMethod("Cash");
     }
 
     @Override
     public void processPayment(Customer customer) {
         // Pretend customer has paid with cash
-        System.out.println("Processing credit card payment for: " + customer.getFullName());
-        System.out.println("Total amount: $" + totalCost);
-        System.out.println("Payment method: " + paymentMethod);
+        LOGGER.info("Processing credit card payment for: " + customer.getFullName());
+        LOGGER.info("Total amount: $" + getTotalCost());
+        LOGGER.info("Payment method: " + getPaymentMethod());
     }
 }
