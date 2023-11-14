62774d4534d7bb2735db551201c612773fdac1cf
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-14 01:41:32 -0500

    Update Email.java

--- src/main/java/com/solvd/gadgetrepair/status/Email.java
+++ src/main/java/com/solvd/gadgetrepair/status/Email.java
@@ -1,8 +1,11 @@
 package com.solvd.gadgetrepair.status;
 
 import com.solvd.gadgetrepair.human.Customer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Email extends Notification implements Notifiable {
+    private static final Logger LOGGER=LogManager.getLogger(Email.class);
     public Email(Notifiable notifiable) {
         super(notifiable);
     }
@@ -10,8 +13,8 @@ public class Email extends Notification implements Notifiable {
     @Override
     public void sendNotification(Customer recipient, String subject, String message) {
         // Pretend an email is actually being sent
-        System.out.println("Sending an email notification to: " + recipient.getEmail());
-        System.out.println("Subject: " + subject);
-        System.out.println("Message: " + message);
+        LOGGER.info("Sending an email notification to: " + recipient.getEmail());
+        LOGGER.info("Subject: " + subject);
+        LOGGER.info("Message: " + message);
     }
 }
