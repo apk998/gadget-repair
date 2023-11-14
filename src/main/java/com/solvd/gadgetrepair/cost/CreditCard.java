696aa07718567512d5fc5a0c7d5b9d74b4ac4a84
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-14 01:45:03 -0500

    Update CreditCard.java

--- src/main/java/com/solvd/gadgetrepair/cost/CreditCard.java
+++ src/main/java/com/solvd/gadgetrepair/cost/CreditCard.java
@@ -1,17 +1,20 @@
 package com.solvd.gadgetrepair.cost;
 
 import com.solvd.gadgetrepair.human.Customer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CreditCard extends Billing implements Payable {
+    private static final Logger LOGGER=LogManager.getLogger(CreditCard.class);
     public CreditCard() {
-        paymentMethod = "Credit card";
+        setPaymentMethod("Credit card");
     }
 
     @Override
     public void processPayment (Customer customer) {
         // Pretend customer has paid with credit card
-        System.out.println("Processing credit card payment for: " + customer.getFullName());
-        System.out.println("Total amount: $" + totalCost);
-        System.out.println("Payment method: " + paymentMethod);
+        LOGGER.info("Processing credit card payment for: " + customer.getFullName());
+        LOGGER.info("Total amount: $" + getTotalCost());
+        LOGGER.info("Payment method: " + getPaymentMethod());
     }
 }
