747c7d88d60146108d275a2edce2caeaa4737eb3
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-14 01:42:00 -0500

    Update SMS.java

--- src/main/java/com/solvd/gadgetrepair/status/SMS.java
+++ src/main/java/com/solvd/gadgetrepair/status/SMS.java
@@ -1,9 +1,11 @@
 package com.solvd.gadgetrepair.status;
 
 import com.solvd.gadgetrepair.human.Customer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SMS extends Notification implements Notifiable {
-
+    private static final Logger LOGGER=LogManager.getLogger(SMS.class);
     public SMS(Notifiable notifiable) {
         super(notifiable);
     }
@@ -11,7 +13,7 @@ public class SMS extends Notification implements Notifiable {
     @Override
     public void sendNotification(Customer recipient, String subject, String message) {
         // Pretend an SMS is actually being sent
-        System.out.println("Sending an email notification to: " + recipient.getPhoneNumber());
-        System.out.println("Message: " + message);
+        LOGGER.info("Sending an email notification to: " + recipient.getPhoneNumber());
+        LOGGER.info("Message: " + message);
     }
 }
