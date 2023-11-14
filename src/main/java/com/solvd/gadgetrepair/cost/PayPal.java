695599163504f56a582c81b60c05c476a5dab171
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-14 01:44:40 -0500

    Update PayPal.java

--- src/main/java/com/solvd/gadgetrepair/cost/PayPal.java
+++ src/main/java/com/solvd/gadgetrepair/cost/PayPal.java
@@ -1,17 +1,20 @@
 package com.solvd.gadgetrepair.cost;
 
 import com.solvd.gadgetrepair.human.Customer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class PayPal extends Billing implements Payable {
+    private static final Logger LOGGER= LogManager.getLogger(PayPal.class);
     public PayPal() {
-        paymentMethod = "PayPal";
+        setPaymentMethod("PayPal");
     }
 
     @Override
     public void processPayment(Customer customer) {
         // Pretend customer has paid via PayPal
-        System.out.println("Processing credit card payment for: " + customer.getFullName());
-        System.out.println("Total amount: $" + totalCost);
-        System.out.println("Payment method: " + paymentMethod);
+        LOGGER.info("Processing credit card payment for: " + customer.getFullName());
+        LOGGER.info("Total amount: $" + getTotalCost());
+        LOGGER.info("Payment method: " + getPaymentMethod());
     }
 }
