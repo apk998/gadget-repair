59e96f1cc7825fc22e9aebc03a69c1a4bdc2cda4
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-15 01:24:50 -0500

    Update PaymentException.java

--- src/main/java/com/solvd/gadgetrepair/exceptions/PaymentException.java
+++ src/main/java/com/solvd/gadgetrepair/exceptions/PaymentException.java
@@ -1,7 +1,7 @@
 package com.solvd.gadgetrepair.exceptions;
 
 // When the customer pays using an unaccepted method
-public class PaymentException extends Exception {
+public class PaymentException extends RuntimeException {
     public PaymentException (String message) {
         super(message);
     }
