6ddae8e023c16940c0ccd3420af015933dcf5cbd
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-15 01:24:08 -0500

    Update GadgetException.java

--- src/main/java/com/solvd/gadgetrepair/exceptions/GadgetException.java
+++ src/main/java/com/solvd/gadgetrepair/exceptions/GadgetException.java
@@ -1,7 +1,7 @@
 package com.solvd.gadgetrepair.exceptions;
 
 // When a customer brings in an unaccepted gadget
-public class GadgetException extends Exception {
+public class GadgetException extends RuntimeException {
     public GadgetException (String message) {
         super(message);
     }
