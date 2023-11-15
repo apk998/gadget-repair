23b798ec8b8b45a04d707a289d56e5da53248cb9
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-15 01:25:01 -0500

    Update UnsupportedPartException.java

--- src/main/java/com/solvd/gadgetrepair/exceptions/UnsupportedPartException.java
+++ src/main/java/com/solvd/gadgetrepair/exceptions/UnsupportedPartException.java
@@ -1,7 +1,7 @@
 package com.solvd.gadgetrepair.exceptions;
 
 // When an unsupported part is requested
-public class UnsupportedPartException extends Exception {
+public class UnsupportedPartException extends RuntimeException {
     public UnsupportedPartException (String message) {
         super(message);
     }
