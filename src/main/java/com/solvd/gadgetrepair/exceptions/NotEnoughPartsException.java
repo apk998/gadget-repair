ff19443aa4a511394a971789bf07df8e6e4fa60a
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-15 01:24:39 -0500

    Update NotEnoughPartsException.java

--- src/main/java/com/solvd/gadgetrepair/exceptions/NotEnoughPartsException.java
+++ src/main/java/com/solvd/gadgetrepair/exceptions/NotEnoughPartsException.java
@@ -1,7 +1,7 @@
 package com.solvd.gadgetrepair.exceptions;
 
 // When the inventory does not have enough requested parts in stock
-public class NotEnoughPartsException extends Exception {
+public class NotEnoughPartsException extends RuntimeException {
     public NotEnoughPartsException (String message) {
         super(message);
     }
