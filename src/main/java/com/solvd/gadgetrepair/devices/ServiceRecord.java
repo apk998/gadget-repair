5419118898eaa81835b9d37aa09097cde45eb57e
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-16 17:19:32 -0500

    Update ServiceRecord.java

--- src/main/java/com/solvd/gadgetrepair/devices/ServiceRecord.java
+++ src/main/java/com/solvd/gadgetrepair/devices/ServiceRecord.java
@@ -1,21 +1,21 @@
 package com.solvd.gadgetrepair.devices;
 
-// Records repair history of each gadget
-public class ServiceRecord {
-    private String repairDate;   // can make datetime object later
+// Records repair history of each customer
+public class ServiceRecord<T> {
+    private T repairDate;   // can be datetime, String, or other object
     private double repairCost;
-    private String repairDetails;
+    private T repairDetails;
 
-    public ServiceRecord(String repairDate, double repairCost, String repairDetails) {
+    public ServiceRecord(T repairDate, double repairCost, T repairDetails) {
         this.repairDate = repairDate;
         this.repairCost = repairCost;
         this.repairDetails = repairDetails;
     }
 
-    public String getRepairDate() {
+    public T getRepairDate() {
         return repairDate;
     }
-    public void setRepairDate(String repairDate) {
+    public void setRepairDate(T repairDate) {
         this.repairDate = repairDate;
     }
     public double getRepairCost() {
@@ -24,10 +24,10 @@ public class ServiceRecord {
     public void setRepairCost(double repairCost) {
         this.repairCost = repairCost;
     }
-    public String getRepairDetails() {
+    public T getRepairDetails() {
         return repairDetails;
     }
-    public void setRepairDetails() {
+    public void setRepairDetails(T repairDetails) {
         this.repairDetails = repairDetails;
     }
 
