99e9ae22916182c15f52ad47b5e4b2e1da3131b7
apk998 <146481462+apk998@users.noreply.github.com>
2023-11-16 17:19:12 -0500

    Update Inventory.java

--- src/main/java/com/solvd/gadgetrepair/devices/Inventory.java
+++ src/main/java/com/solvd/gadgetrepair/devices/Inventory.java
@@ -4,26 +4,25 @@ import com.solvd.gadgetrepair.exceptions.InventoryFullException;
 import com.solvd.gadgetrepair.exceptions.NotEnoughPartsException;
 import com.solvd.gadgetrepair.exceptions.UnsupportedPartException;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 // Stores the stock of spare parts and tools for repair
 public class Inventory {
-    private final String[] parts;
-    private final int[] quantities;
+    private final Set<String> parts;
+    private final Map<String, Integer> quantities;
     public static final int MAX_CAPACITY = 5000;
     public static final int DEFAULT_PART_QUANTITY = 10;
 
     public Inventory(int capacity) {
-        parts = new String[capacity];
-        quantities = new int[capacity];
+        parts = new HashSet<>();
+        quantities = new HashMap<>();
     }
 
     public boolean isInventoryFull() {
-        int totalParts = 0;
-        for (String part : parts) {
-            if (part != null) {
-                totalParts++;
-            }
-        }
-        return totalParts >= MAX_CAPACITY;
+        return parts.size() >= MAX_CAPACITY;
     }
 
     public void addPart(String part, int quantity) throws InventoryFullException {
@@ -31,41 +30,34 @@ public class Inventory {
             throw new InventoryFullException("Inventory is full. Cannot add part(s).");
         }
 
-        for (int i=0; i < parts.length; i++) {
-            if (parts[i] == null) {
-                parts[i] = part;
-                quantities[i] = quantity;
-                break;
-            }
-        }
+        parts.add(part);
+        quantities.put(part, quantity);
     }
 
     public void removePart(String part, int quantity) throws NotEnoughPartsException, UnsupportedPartException {
-        int partIndex = -1;
-        for (int i=0; i < parts.length; i++) {
-            if (parts[i] != null && parts[i].equals(part)) {
-                partIndex = i;
-                break;
+        if (parts.contains(part)) {
+            int currentQuantity = quantities.get(part);
+
+            if (currentQuantity < quantity) {
+                throw new NotEnoughPartsException("Not enough parts in stock.");
             }
-        }
-        if (partIndex == -1) {
+
+            currentQuantity -= quantity;
+            quantities.put(part, currentQuantity);
+
+            if (currentQuantity == 0) {
+                parts.remove(part);
+            }
+        } else {
             throw new UnsupportedPartException("Part not recognized or supported.");
-        }
-        if (quantities[partIndex] < quantity) {
-            throw new NotEnoughPartsException("Not enough parts in stock.");
-        }
-        quantities[partIndex] -= quantity;
-        if (quantities[partIndex] == 0) {
-            parts[partIndex] = null;
         }
     }
 
     public int getQuantity(String part) {
-        for (int i = 0; i < parts.length; i++) {
-            if (part.equals(parts[i])) {
-                return quantities[i];
-            }
-        }
-        return -1;   // if the part is not found
+        return quantities.getOrDefault(part, -1);   // -1 if the part is not found
+    }
+
+    public Set<String> getUniqueParts() {
+        return parts;
     }
 }
